30504c578cb1a0128a493eaa0d73d1ec6e65a266
Witkowski Antoine <witkowski.antoine@gmail.com>
2024-03-10 21:26:01 +0100

    update BMI calculator

--- src/imc_calculator/src/main.rs
+++ src/imc_calculator/src/main.rs
@@ -1,3 +1,22 @@
+extern crate tools;
+
 pub fn main() {
-    println!("IMC Calcutor");
+    println!("Welcome into the Body Mass Index Calculator");
+    let weight = weight();
+    let height = height();
+    println!("Your Body Mass Index is {}", weight / (height * height));
+}
+
+fn weight() -> f32 {
+    println!("What is your weight in kg with decimal (ex: 70.0 for 70kg) ?");
+    let weight = tools::read_f32();
+    println!("{} kg", weight);
+    return weight;
+}
+
+fn height() -> f32 {
+    println!("What is your height in meter (ex: 1.8 for 1meter80) ?");
+    let height = tools::read_f32();
+    println!("{} m", height);
+    return height;
 }
