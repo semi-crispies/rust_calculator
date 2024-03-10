0f848154721234160d05e6b2d69cc657ed60bd24
Witkowski Antoine <witkowski.antoine@gmail.com>
2024-03-10 19:23:13 +0100

    use imc_calculator in main app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+extern crate imc_calculator;
+
 fn main() {
-    println!("Hello, world!");
+    println!("What do you want?\n 1. Infinite Calculator\n 2. IMC Calculator");
+    let input = read_u8();
+    match input {
+        1 => println!("Infinite Calculator"),
+        2 => imc_calculator::main(),
+        n => println!("T'es le roi des CONNARDS !!!!!!!"),
+    }
+
+    fn read_u8() -> u8 {
+        let input = io::stdin().lines().next().unwrap().unwrap();
+        input.parse().unwrap()
+    }
 }
