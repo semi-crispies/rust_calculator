be482ed62e874e11d5005e3526c27187dd233ba3
Witkowski Antoine <witkowski.antoine@gmail.com>
2024-03-10 21:28:26 +0100

    rename IMC into BMI

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 extern crate tools;
-extern crate imc_calculator;
+extern crate bmi_calculator;
 
 fn main() {
     println!("What do you want?\n 1. Infinite Calculator\n 2. IMC Calculator");
     let input = tools::read_u8();
     match input {
         1 => println!("Infinite Calculator"),
-        2 => imc_calculator::main(),
+        2 => bmi_calculator::main(),
         n => println!("T'es le roi des CONNARDS !!!!!!!"),
     }
 }
