3afcbaf7fca2f4d7f49960d9aed29e20a6db32d6
Witkowski Antoine <witkowski.antoine@gmail.com>
2024-03-10 21:25:29 +0100

    use tools

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
-use std::io;
+extern crate tools;
 extern crate imc_calculator;
 
 fn main() {
     println!("What do you want?\n 1. Infinite Calculator\n 2. IMC Calculator");
-    let input = read_u8();
+    let input = tools::read_u8();
     match input {
         1 => println!("Infinite Calculator"),
         2 => imc_calculator::main(),
         n => println!("T'es le roi des CONNARDS !!!!!!!"),
-    }
-
-    fn read_u8() -> u8 {
-        let input = io::stdin().lines().next().unwrap().unwrap();
-        input.parse().unwrap()
     }
 }
